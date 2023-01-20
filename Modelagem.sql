338c24a2d6e5ba724c6d4881b589caa3ea7325d8
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-20 11:07:30 -0300

    Update Modelagem.sql

--- Modelagem.sql
+++ Modelagem.sql
@@ -1,2 +1,23 @@
 -- MODELAGEM
 
+SELECT sexo, COUNT(*) as QTD 
+FROM cliente
+GROUP BY sexo;
+
+
+-- PRIMEIRA FORMA NORMAL  (1FN)
+
+/* 
+1. Todo campo vetorizado se tornará outra tabela
+	pertencem a mesma 'família'
+	
+	Ex: Telefone
+
+
+2. Todo campo multivalorado se tornará outra tabela
+	Quando o campo for divisível 
+	
+	Ex: Endereço  >>> Rua, Bairro, Cidade, Estado
+
+
+*/
