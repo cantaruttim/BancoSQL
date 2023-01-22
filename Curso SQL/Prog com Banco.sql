c09bffe2512397bfbfd6034b1f35af83b735d506
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-22 16:09:52 -0300

    Update Prog com Banco.sql

--- Curso SQL/Prog com Banco.sql
+++ Curso SQL/Prog com Banco.sql
@@ -2,4 +2,16 @@
 
 
 ; : Delimitador do banco de dados
-Portando o ; separa os blocos de comando do MySQL */
+Portando o ; separa os blocos de comando do MySQL
+
+
+STATUS
+Using Delimiter : precisamos mudar o delimitador
+
+muda o delimitador para $ por exemplo (podemos fazer qualquer outro comando)
+DELIMITER $
+
+Uptime : Tempo referente ao tempo que o seu banco está de pé, desde a 
+ultima vez que ele executor
+
+ */
