abf3ac3a81b301d3cc67e4bd9067989c7bbec5af
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-22 16:24:20 -0300

    Update Prog com Banco.sql

--- Curso SQL/Prog com Banco.sql
+++ Curso SQL/Prog com Banco.sql
@@ -14,4 +14,73 @@ DELIMITER $
 Uptime : Tempo referente ao tempo que o seu banco está de pé, desde a 
 ultima vez que ele executor
 
+
+###############################
+
+STORED PROCEDURES:
+
+-- Blocos anônimos e Armazenados (possuem nome)
+
+Toda vez que alguém logar no banco, precisamos mostrar o nome da empresa
+-- A mudança do delimitador é importante, pois caso não haja a mudança
+o procedure não irá acontecer, ou o código será lido de forma incompleta.
+
+DELIMITER $
+
+CREATE PROCEDURE NOME()
+BEGIN
+
+	<
+	
+	
+	
+	bloco de código 
+	
+	
+	
+	
+	>	
+
+END
+$
+
+
+
  */
+ 
+DELIMITER $
+ 
+CREATE PROCEDURE NOME_EMPRESA()
+BEGIN
+
+	SELECT 'UNIVERSIDADE DOS DADOS' AS EMPRESA;
+	SELECT NOW();
+	
+END
+$
+
+-- Chamando a PROCEDURE
+
+CALL NOME_EMPRESA()$
+
+-- DELETEANDO UMA PROCEDURE
+
+DROP PROCEDURE NOME_EMPRESA;
+
+DELIMITER ;
+
+-- passando parâmetros:
+
+CREATE PROCEDURE CONTA(NUMERO1 INT, NUMERO2 INT)
+BEGIN
+
+	SELECT NUMERO1 + NUMERO2 AS CONTA;
+	
+END
+$
+
+CALL CONTA(25, 150)$
+CALL CONTA(5, 20)$
+
+
+
