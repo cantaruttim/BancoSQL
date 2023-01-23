971f1048c3e6935ccff81fbba7a8fae62f5125a6
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-23 17:38:02 -0300

    Update TRIGGERS.sql

--- Curso SQL/TRIGGERS.sql
+++ Curso SQL/TRIGGERS.sql
@@ -1,4 +1,4 @@
-/* TRIGGERS */
+/* TRIGGERS 
 
 DELIMITER $
 
@@ -13,7 +13,52 @@ END
 $
 
 
+Obs¹: Temos 6 momentos em que podemos colocar uma TRIGGER
+para disparar no banco. Para cada operação (INSERT,DELETE,UPDATE) Antes e Depois.
+Com isso não podemos ter mais de uma trigger olhando para o mesmo momento em uma mesma tabela!
+
+Obs²: Os dados que estão sendo inseridos são 'novos', NEW.
+Já os dados que já estão armazenados na tabela são 'velhos', OLD.
+
+*/
 use information_schema;
 show tables;
 
 DESC TRIGGERS;
+
+
+CREATE DATABASE ATRIGGGERS;
+USE ATRIGGGERS;
+
+
+CREATE TABLE USUARIO(
+	IDUSUARIO INT PRIMARY KEY AUTO_INCREMENT,
+	NOME VARCHAR(30),
+	LOGIN VARCHAR(30),
+	SENHA VARCHAR(100)
+);
+
+-- QUERO FAZER UM BACKUP IMEDIATO APÓS A EXCLUSÃO DE QQ USUÁRIO
+
+
+CREATE TABLE BKP_USUARIO(
+	IDBKUP INT PRIMARY KEY AUTO_INCREMENT,
+	IDUSUARIO INT,
+	NOME VARCHAR(30),
+	LOGIN VARCHAR(30)
+);
+
+
+-- CRIANDO A TRIGGER
+
+DELIMITER $
+
+CREATE TRIGGER BACKUP_USUARIO 
+BEFORE DELETE ON  USUARIO
+	FOR EACH ROW
+BEGIN 
+
+	
+	
+END
+$
