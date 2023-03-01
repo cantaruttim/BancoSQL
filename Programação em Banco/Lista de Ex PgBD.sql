054bb0557c00117e613f53a0f73c965347f55e3b
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-01 14:38:32 -0300

    Ex 1 corrigidos

--- Programação em Banco/Lista de Ex PgBD.sql
+++ Programação em Banco/Lista de Ex PgBD.sql
@@ -44,16 +44,16 @@ $$
 DECLARE
 	temp NUMERIC (5,2);
 	F NUMERIC (5,2);
-	li INTEGER (20,2) := 20;
-	lf INTEGER (30,0) := 30;
+	li INTEGER := 20;
+	ls INTEGER := 30;
 BEGIN
 	-- Gerando um valor aleatório entre 20 e 30.
 	temp := FLOOR(li + random() * (ls - li + 1));
-	-- RAISE NOTICE '%', temp;
+	RAISE NOTICE 'A temperatura em Cº é: %', temp;
 
 	-- Convertendo a temperatura C° > F°
 	F := (temp *1.8) + 32;
-	RAISE NOTICE '%', F;	
+	RAISE NOTICE 'A temperatura em Fº é: %', F;	
 END;
 $$
 
@@ -61,6 +61,9 @@ $$
 /* 4.  Faça um programa que gere três valores reais a, b, e c e 
 mostre o valor de delta: aquele que calculamos para chegar às potenciais 
 raízes de uma equação do segundo (/\ = b² - 4.a.c) */
+
+
+-- CORRIGIR 
 
 DO 
 $$
@@ -74,8 +77,8 @@ DECLARE
 	delta NUMERIC (5,2);
 	
 	-- determinando o range dos números
-	li INTEGER (20,2) := 20;
-	lf INTEGER (30,0) := 30;
+	li INTEGER := 20;
+	ls INTEGER := 30;
 BEGIN
 	
 	-- primeiro vamos gerar os 3 valores para a,b e c
@@ -83,7 +86,7 @@ BEGIN
 	b := FLOOR(li + random() * (ls - li + 1));
 	c := FLOOR(li + random() * (ls - li + 1));
 	
-	delta := b ^ 2 (- 4 * a * c);
+	delta := b ^ 2 -4 * a * c;
 	RAISE NOTICE '% ^ % (% * % * %) = %', b^2, -4*a*c, delta;
 	
 END;
@@ -92,6 +95,7 @@ $$
 
 /* 5. Faça um programa que gere um número inteiro e 
 mostre a raiz cúbica de seu antecessor e a raiz quadrada de seu sucessor.*/
+
 
 DO
 $$
@@ -105,9 +109,8 @@ BEGIN
 	-- gerar o número dentro do intervalo
 	n := FLOOR(li + random() * (ls - li + 1));
 	
-	CASE WHEN n + 1 THEN '|/ % = %', n, |/ n -- sucessor
-		 WHEN n - 1 THEN '||/ % = %', n, ||/ n -- antecessor
-	END
+	RAISE NOTICE 'Sucessor: % + % = %', n, n + 1, |/ n;
+	RAISE NOTICE 'Antecessor: % - % = %', n, n - 1, ||/ n;
 END;
 $$
 
@@ -121,17 +124,17 @@ $$
 
 DECLARE
 	-- variáveis do retângulo
-	comp NUMERIC (5,2);
-	alt NUMERIC (5,2);
-	area NUMERIC (5,2);
-	metragem NUMERIC (5,2); -- vai ser o valor que é baseado nos limites
-	total_terreno NUMERIC (5,2);
+	comp NUMERIC (60,2);
+	alt NUMERIC (60,2);
+	area NUMERIC (60,2);
+	metragem NUMERIC (60,2); -- vai ser o valor que é baseado nos limites
+	total_terreno NUMERIC (60,2);
 	
-	preco NUMERIC(5,2);
+	preco NUMERIC(60,2);
 	
 	-- limites
-	li_terreno := 1;
-	ls_terreno := 100;
+	li_terreno INTEGER := 1;
+	ls_terreno INTEGER := 100;
 	
 	-- limites da metragem
 	li INTEGER := 60;
@@ -144,14 +147,15 @@ BEGIN
 	RAISE NOTICE 'A área do terreno é: %', area;
 	
 	-- valor da metragem (representa o preço do terreno por metro)
-	metragem := li + random() * (ls - li +1)
+	metragem := li + random() * (ls - li +1);
 	RAISE NOTICE 'O valor da metragem do terreno é de: %', metragem;
 	
 	-- mostrando o valor total do terreno (metro por area)
-	preco := metragem * area
+	preco := metragem * area;
 	RAISE NOTICE 'O valor total do terreno é: R$ %', preco;
 END;
 $$
+
 
 /* 7. Escreva um programa que gere um inteiro que representa o 
 ano de nascimento de uma pessoa no intervalo [1980, 2000]
@@ -170,12 +174,13 @@ DECLARE
 	-- limites
 	ano_nasc_liI INTEGER := 1980;
 	ano_nasc_liS INTEGER := 2000;
-	ano_atual_liI INTEGER 2010;
-	ano_atual_liS INTEGER 2020;
+	ano_atual_liI INTEGER := 2010;
+	ano_atual_liS INTEGER := 2020;
 BEGIN
 	-- anos
-	ano_nascimento := FLOOR(ano_nasc_liI + random() * (ano_nasc_liS - ano_nasc_liI + 1);
-	ano_atual := FLOOR(ano_atual_liI + random() * (ano_atual_liS - ano_atual_liI + 1))
+	ano_nascimento := FLOOR(ano_nasc_liI + random() * (ano_nasc_liS - ano_nasc_liI + 1));
+	ano_atual := FLOOR(ano_atual_liI + random() * (ano_atual_liS - ano_atual_liI + 1));
+	RAISE NOTICE 'O ano de nascimento é % e o ano atual é %', ano_nascimento, ano_atual;
 	-- exibindo a idade em anos
 	
 	idade := ano_atual - ano_nascimento;
