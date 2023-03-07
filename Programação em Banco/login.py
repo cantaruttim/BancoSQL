2321a615f9a3eef35588cb2f79f5e217220f382a
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-07 11:06:57 -0300

    Add files via upload

--- Programação em Banco/login.py
+++ Programação em Banco/login.py
@@ -2,11 +2,10 @@ import psycopg
 print(psycopg)
 
 # Criando a classe usuário
-class usuario:
+class Usuario:
     def __init__(self, login, senha):
         self.login = login
         self.senha = senha
-
 
 #verificando se o usuário existe
 def existe(usuario):
@@ -14,7 +13,8 @@ def existe(usuario):
         host='localhost',
         port=5432,
         dbname='postgres',
-        password='123456'
+        password='123456',
+        user='postgres'
     ) as conexao:
         with conexao.cursor() as cursor:
             cursor.execute('SELECT * FROM tb_usuario WHERE login=%s AND senha=%s', (f'{usuario.login}',
@@ -29,27 +29,29 @@ def existe(usuario):
 def menu():
     texto="0-Fechar Sistema\n1-Login\n2-Logoff\n"    
     #usuário ainda não existe
-    usuario=None
+    usuario = None
     #capturamos a opção do usuário
-    op=int(input(texto))
+    op= int(input(texto))
             
     while op != 0:
         
         if op==1:
             login = input("Digite seu login\n")
             senha = input("Digite sua senha\n")
-            usuario = usuario(login,senha)
-            print("Usuário OK!!!" if existe(usuario) else "Usuário OK!!!")
+            usuario = Usuario(login,senha)
+            print("Usuário OK!!!" if existe(usuario)
+                                    else "Usuário OK!!!")
             
-        #se ele digitar 2, configuramos o usuariocomo "None" novamente
+        #se ele digitar 2, configuramos o usuario como "None" novamente
             
         elif op == 2:
             usuario = None
             print("Logoff realizado com sucesso")
             
             op=int(input(texto))
+
     else:
-        #se digitar zero, dizemos adeus. Observe queesse else está associado ao while
+        #se digitar zero, dizemos adeus. Observe que esse else está associado ao while
         print("Até mais")
 #chamamos a função menu
 menu()
