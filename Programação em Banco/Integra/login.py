de20b7761da94e972458b6ddbd5b6f644d2dcd9b
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-03-07 21:37:37 -0300

    files

---
+++ Programação em Banco/Integra/login.py
@@ -0,0 +1,61 @@
+import psycopg
+from usuario import Usuario
+#  print(psycopg)
+
+
+# verificando se o usuário existe
+def existe(usuario):
+    with psycopg.connect(
+            host='localhost',
+            port=5432,
+            dbname='Aulas',
+            password='123456',
+            user='postgres'
+    ) as conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute('SELECT * FROM tb_usuario WHERE login=%s AND senha=%s',
+                           (f'{usuario.login}', f'{usuario.senha}'))
+
+            result = cursor.fetchone()
+
+    return result != None
+print(existe(Usuario('matheus', 'admin')))
+
+# definição da função menu
+
+
+
+def menu():
+    texto = "0-Fechar Sistema\n1-Login\n2-Logoff\n3-Cadastrar"
+    # usuário ainda não existe
+    usuario = None
+    # capturamos a opção do usuário
+    op = int(input(texto))
+
+    while op != 0:
+
+        if op == 1:
+            login = input("Digite seu login\n")
+            senha = input("Digite sua senha\n")
+            usuario = Usuario(login, senha)
+
+            print("Usuário OK!!!" if existe(usuario)
+                  else "Usuário OK!!!")
+
+        # se ele digitar 2, configuramos o usuario como "None" novamente
+
+        elif op == 2:
+            usuario = None
+            print("Logoff realizado com sucesso")
+
+            op = int(input(texto))
+
+        ## Faça um bloco de código que insira um valor no banco
+
+    else:
+        # se digitar zero, dizemos adeus. Observe que esse else está associado ao while
+        print("Até mais")
+
+
+# chamamos a função menu
+menu()
