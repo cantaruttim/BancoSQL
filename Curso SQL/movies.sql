f91cf53c98324703c87c48e83f726d9ce3da7aea
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-12 22:14:42 -0300

    Add files via upload

---
+++ Curso SQL/movies.sql
@@ -0,0 +1,10 @@
+CREATE TABLE actors(
+	actors_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(150),
+	last_name VARCHAR(150) NOT NULL,
+	gender CHAR(1),
+	date_of_birth DATE,
+	add_date DATE,
+	update_date DATE
+);
+
