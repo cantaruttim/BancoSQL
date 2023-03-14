2e437133890dac3d6a830fdd1c87e4058a12c937
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-14 10:45:28 -0300

    Add files via upload

--- Programação em Banco/Estruturas_de_Selecao.sql
+++ Programação em Banco/Estruturas_de_Selecao.sql
@@ -1,2 +1,24 @@
--- Operadores Lógicos (14/03)
+/* Operadores Lógicos (14/03)
+-- AND OR NOT */
+
+SELECT 2 > 3 AS "Teste Lógico";
+SELECT 2 < 3 AS "Teste Lógico";
+SELECT 3 = 3 AS "Teste Lógico";
+SELECT 1 <= 2 AS "Teste Lógico";
+SELECT 1 >= 2 AS "Teste Lógico";
+SELECT 1 <> 2 AS "Teste Lógico";
+
+SELECT 2 BETWEEN 1 AND 3 AS "INTERVALO";
+SELECT 2 BETWEEN SYMMETRIC 3 AND 1 AS "INTERVALO"; -- Igual a 1 à 3 
+SELECT 2 IS DISTINCT FROM NULL; -- Valores diferentes de "desconhecido"
+SELECT 2 ISNULL;
+SELECT 2 NOTNULL;
+
+-- Estruturas de De Seleção
+
+
+
+
+
+
 
