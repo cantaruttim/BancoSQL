b18fb4218c94f8ae5653c8694d1a81b22411cb1a
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-03-14 21:49:35 -0300

    Update Exercicios_Selecao.sql

--- Programação em Banco/Exercicios_Selecao.sql
+++ Programação em Banco/Exercicios_Selecao.sql
@@ -3,10 +3,23 @@
 -- 1. Faça um programa que mostre se o número é multiplo de 3 (três) [M = n . k, k E Z]
 
 DO $$
-DECLARE 
-
+DECLARE
+	n INTEGER := valor_aleatorio_entre(0,5);
+	m INTEGER := valor_aleatorio_entre(0,5);
+	k INTEGER;
 BEGIN
-
+	RAISE NOTICE 'Os valores gerados de m e n são : %, % respectivamente', m, n;
+	k := m * n;
+	RAISE NOTICE 'A multiplicação entre % e % é = %', m, n, k;
+	
+	IF (k/m) <> 0 THEN
+		-- Se (k / m) = n dizemos que o valor é multiplo 
+		IF (k/m) = 3 THEN
+			RAISE NOTICE 'Multiplo de 3';
+		ELSE
+			RAISE NOTICE 'Não é multiplo de 3';
+		END IF;
+	END IF;
 END; $$
 
 -- 2. Faça um programa que exibe se um número inteiro é multiplo de 3 ou de 5.
