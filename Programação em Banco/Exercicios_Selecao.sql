09cca06ebbdd7e8c1500514c82c7236cddad5283
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-03-15 09:27:17 -0300

    Ex 2 e 3

--- Programação em Banco/Exercicios_Selecao.sql
+++ Programação em Banco/Exercicios_Selecao.sql
@@ -25,11 +25,25 @@ END; $$
 -- 2. Faça um programa que exibe se um número inteiro é multiplo de 3 ou de 5.
 
 DO $$
-DECLARE 
-
+DECLARE
+	n INTEGER := valor_aleatorio_entre(0,5);
+	m INTEGER := valor_aleatorio_entre(0,5);
+	k INTEGER;
 BEGIN
-
+	RAISE NOTICE 'Os valores gerados de m e n são : %, % respectivamente', m, n;
+	k := m * n;
+	RAISE NOTICE 'A multiplicação entre % e % é = %', m, n, k;
+	
+	IF (k/m) <> 0 THEN
+		-- Se (k / m) = n dizemos que o valor é multiplo 
+		IF (k/m) = 3 OR (k/m) = 5 THEN
+			RAISE NOTICE 'Multiplo de 3 ou 5';
+		ELSE
+			RAISE NOTICE 'Não é multiplo de 3 ou de 5';
+		END IF;
+	END IF;
 END; $$
+
 
 /* 3. Faça um programa que opere de acordo com o seguinte menu
 	1 - Soma
@@ -37,6 +51,31 @@ END; $$
 	3 - Multiplicação
 	4 - Divisão
 */
+
+DO $$
+DECLARE 
+	n1 INT := valor_aleatorio_entre (1,5);
+	n2 INT := valor_aleatorio_entre (1,5);
+	res INT (4,2);
+	
+	-- menu
+	op_soma CHAR := '+';
+	op_subtracao CHAR := "-";
+	op_multiplicacao CHAR := "*";
+	op_divisao CHAR := "/";
+BEGIN
+	RAISE NOTICE 'Os valores de n1 e n2 são : % e %', n1, n2;
+
+	-- resultado deve ser apresentado como n1 op n2 = res
+
+
+
+
+
+
+
+END; $$
+------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION valor_aleatorio_entre (
 	lim_inferior INT, lim_superior INT
